2fa02aaafdac1e7e67d6165908c6a0b0e57cf3f3
nourbedoui1000-alt <nourbedoui1000@gmail.com>
2026-04-26 15:49:53 +0100

    feat: ajout des routes d'authentification (login, register, logout) avec middlewares

--- platforme-coworking2/routes/web.php
+++ platforme-coworking2/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware('guest:client')->group(function () {
+    Route::get('/login',    [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login',   [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register',[AuthController::class, 'register']);
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:client')
+    ->name('logout');
